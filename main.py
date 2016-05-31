ac61a6f0629b08fa3f9c0a559e86bc9e4b5eeacc
Ryan Kelly <ryankell@umich.edu>
2016-05-30 21:16:45 -0500

    Added services page

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ app.config['DEBUG'] = True
 def main_route():
   options = { "message": "hello world" }
   return render_template("index.html", **options)
+
+@app.route('/services')
+def services_route():
+	options = { "messages": "Hello services" }
+	return render_template("services.html", **options)
